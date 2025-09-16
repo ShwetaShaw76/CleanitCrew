4b29e6c7d7b0a7ec2c6e0039e94b24dea8a9879c
ShwetaShaw76 <189984154+ShwetaShaw76@users.noreply.github.com.>
2025-09-16 23:22:59 +0530

    made a footer and added some effects to it

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <footer>
-    <article>Made by Shweta </article>
+    <article>Made by Shweta for Clutter(Hackclub)</article>
+    <aside>See More of my projects on my github page --</aside>
+    <ul>
+        <li><a href="https://github.com/ShwetaShaw76"><img src="https://icons.iconarchive.com/icons/bokehlicia/captiva/256/web-github-icon.png" alt="github"></a></li>
+    </ul>
 </footer>
 
 <style>
@@ -11,10 +15,12 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        
+        flex-direction: column;
         padding: 10px 20px;
-        
-        background-color: #222;
+        margin-top: 3vh;
+        background: linear-gradient(to right, #fbb1d9, #caa0ff);
+        animation: back 10s infinite;
+        text-align: center;
         color: white;
         font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
         font-weight: 700;
@@ -23,7 +29,22 @@
         bottom: 0;
         left: 0;
     }
+    @keyframes back{
+        0%{background: linear-gradient(to right, #fbb1d9, #caa0ff);}
+        20%{background: linear-gradient(to right, #caa0ff, #fbb1d9);}
+        40%{background: linear-gradient(to right, #fbb1d9, #caa0ff);}
+        60%{background: linear-gradient(to right, #caa0ff, #fbb1d9);}
+        80%{background: linear-gradient(to right, #fbb1d9, #caa0ff);}
+        100%{background: linear-gradient(to right, #caa0ff, #fbb1d9);}
+    }
     article{
         font-size: 3rem;
+    }
+    aside{
+        font-size: 1.5rem;
+        margin-top: 10px;
+    }
+    ul li a img{
+        width: 4vw;
     }
 </style>
